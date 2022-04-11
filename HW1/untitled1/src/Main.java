3d213dc0e5ec3c3af63bef1eb80bdcc71ed28142
chamchi-god <dongwonida@snu.ac.kr>
2022-04-11 16:53:02 +0900

    Update Main.java

--- HW1/untitled1/src/Main.java
+++ HW1/untitled1/src/Main.java
@@ -3,16 +3,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-        move(5);
-        System.out.println(count);
+        int[] array = {1,4, 300, 10, 7, 9,3,5}; //array 받아들이기, 시간 측정
+        RandomSelect randomSelect = new RandomSelect();
+        DeterministicSelect andomSelect = new DeterministicSelect();
+        System.out.println(randomSelect.randomSort(array, 3));
+        CheckerProgram checker = new CheckerProgram();
+        checker.checkerProgram(array, randomSelect.randomSort(array, 3), 3);
+
     }
-    public static long count = 0;
-    public static int move(int k){
-        count++;
-       if (k==1 || k==2){
-           return 1;
-       } else{
-           return move(k-1)+move(k-2);
-       }
-    }
+
 }
