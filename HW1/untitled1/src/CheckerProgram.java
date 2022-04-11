ac3c3f223a77639b5d20e6bcb493fc1eb0d5feb1
chamchi-god <dongwonida@snu.ac.kr>
2022-04-11 16:52:55 +0900

    Update CheckerProgram.java

--- HW1/untitled1/src/CheckerProgram.java
+++ HW1/untitled1/src/CheckerProgram.java
@@ -1,12 +1,12 @@
 public class CheckerProgram {
-    public void checkerProgram(int[] array){
-        int n=0;
-        for (int i=0; i<array.length-1; i++){
-            if (array[i]<array[i+1]){
-                n++;
+    public void checkerProgram(int[] array, int k, int n){
+        int s = 0;
+        for (int i=0; i<array.length; i++){
+            if (array[i]<k){
+                s++;
             }
         }
-        if (n == array.length-1){
+        if (s+1 ==n){
             System.out.println("선형시간 완료");
         }
     }
